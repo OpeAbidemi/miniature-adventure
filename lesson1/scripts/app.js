7f5e896c861b939d1ac31c3fe2e5bb7d301cffb8
Abidemi Ope <opeabidemi@gmail.com>
2022-09-21 11:21:53 +0100

    new lesson

---
+++ lesson1/scripts/app.js
@@ -0,0 +1,18 @@
+// alert("Hello World");
+// alert("My name is james bond");
+// prompt("Enter your name");
+// confirm("Are you sure you want to exit?");
+// open("https://google.com");
+
+// console.log("This is logged to the console");
+// console.warn("This is a warning");
+// console.error("This is an error");
+
+// console.time("test");
+// alert("Hello World");
+// console.timeEnd("test");
+
+// DOM - Document Object Model
+
+document.body.style.backgroundColor = "red";
+document.getElementById("heading").style.color = "blue";
