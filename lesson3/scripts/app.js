296b31128a7449492a74b6d0a9c8b336f519907f
Abidemi Ope <opeabidemi@gmail.com>
2022-09-26 20:24:22 +0100

    new lesson

---
+++ lesson3/scripts/app.js
@@ -0,0 +1,67 @@
+// // Variables
+// var firstName; // Defining a variable
+// firstName = "James"; // Initializing
+
+// // console.log(firstName);
+
+// // var age = 30;
+
+// // console.log(age);
+
+// {
+//   let firstName = "John";
+//   var gender = "male";
+//   //   console.log(window.firstName);
+// }
+
+// // console.log(gender);
+
+// const PI = 3.142; // This is a constant
+
+// // PI = 4.5;
+
+// gender = "female";
+
+// Data Types
+
+// - Primitive Data Types
+// number
+// string
+// boolean
+// functions
+
+// - Reference Data Types
+// Arrays
+// Objects
+// Classes
+
+let score = 3000.5678; // number
+let surname = "Chen"; // string
+let isDark = false; // boolean
+
+// surname = 20;
+
+// console.log(typeof surname);
+// console.log(typeof isDark);
+
+// Number Methods
+
+// console.log(score);
+// console.log(score.toFixed(1));
+// console.log(score.toPrecision(1));
+// console.log(score.toExponential(1));
+// console.log(score.toString());
+
+// String Methods
+
+let countryName = "Afghanistan";
+console.log(countryName.length);
+console.log(countryName.endsWith("en"));
+console.log(countryName.startsWith("a"));
+console.log(countryName.includes("ni"));
+console.log(countryName.includes("ni", 9)); // with position
+console.log(countryName.charAt(4));
+console.log(countryName.indexOf("a"));
+console.log(countryName.lastIndexOf("a"));
+console.log(countryName.concat(" Kabul"));
+let ageOfDocument = 30;
