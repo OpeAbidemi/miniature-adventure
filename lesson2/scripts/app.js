afab110f8543ac3538d2553339b79e87b24737fa
Abidemi Ope <opeabidemi@gmail.com>
2022-09-26 10:09:36 +0100

    old lesson

---
+++ lesson2/scripts/app.js
@@ -0,0 +1,16 @@
+// console.log(document.getElementById("greeting").innerHTML);
+// // console.log(document.getElementById("greeting2"));
+
+// document.getElementById("greeting").innerText =
+//   "<a href='https://google.com'>Hello James</a>";
+
+// console.log(document.getElementById("greeting").innerHTML);
+
+console.log(document.getElementsByClassName("box"));
+console.log(document.getElementsByTagName("h3"));
+console.log(document.getElementsByName("gender"));
+
+console.log(document.querySelector("form input"));
+console.log(document.querySelectorAll("div.box"));
+
+document.querySelector("#greeting").style.transform = "scale(1.05)";
