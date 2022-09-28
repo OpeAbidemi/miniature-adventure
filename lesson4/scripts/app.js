cb4e62f9914ab4cd5137885c0d822110b7315291
Abidemi Ope <opeabidemi@gmail.com>
2022-09-28 11:35:30 +0100

    new lesson

---
+++ lesson4/scripts/app.js
@@ -0,0 +1,65 @@
+// const firstname = prompt("Enter your name");
+// const age = parseInt(prompt("Enter your age"));
+
+// const PI = parseFloat(prompt("Enter the value of PI"));
+
+// console.log(firstname);
+// console.log(age);
+// console.log(PI);
+
+// Operators
+
+// Arithmetic Operators
+// Assignment //
+// Relational Operators
+// Logical Operators
+// Unary Operators
+// Ternary //
+
+// Arithmetic Operators
+
+let num1 = 20,
+  num2 = 30;
+
+// console.log(num1 + num2); // Add
+// console.log(num1 - num2); // Sub
+// console.log(num1 * num2); // Mult
+// console.log(num1 / num2); // Division
+// console.log(num2 % num1); // Modulus
+// console.log(num2 ** 3); // Exponential
+
+// Assignment Operators
+
+// num1 += 2; // num1 = num1 + 2
+// console.log(num1);
+// num2 -= 4; // num2 = num2 - 4
+// console.log(num2);
+// num1 *= num2; // num1 = num1 * num2
+// console.log(num1);
+// num2 /= 4; // num2 / 4
+
+// Relational Operators
+// let num1 = 20,
+//   num2 = 30;
+
+// console.log(num2 != num1);
+
+//Logical Operators
+//  AND, OR, NOT
+
+// AND = &&
+// OR = ||
+// NOT = !
+
+// console.log(!(num2 < num1)); // true
+
+// Unary Operators
+
+// Increment and Decrement
+
+// Post = num1++
+// Pre = ++num1
+
+console.log(num1);
+console.log(--num1);
+console.log(num1);
